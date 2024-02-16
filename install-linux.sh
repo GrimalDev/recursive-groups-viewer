12af80c5742645b2f9d24d73a5ae31d765d8b604
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2024-02-16 11:49:11 +0100

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -1,9 +1,24 @@
-echo "Downloading the tool from github...\n"
-curl -s https://raw.githubusercontent.com/GrimalDev/group-manager/main/groupmng.py -o groupmng.py
-echo "Done\n"
+#!/bin/bash
 
-echo "Instaling the tool..."
+# ASCII Title
+echo -e "\e[95m
+ _______                                                                       
+(_______)                                                                      
+ _   ___  ____ ___  _   _ ____ _____ ____  _____ ____  _____  ____ _____  ____ 
+| | (_  |/ ___) _ \| | | |  _ (_____)    \(____ |  _ \(____ |/ _  | ___ |/ ___)
+| |___) | |  | |_| | |_| | |_| |    | | | / ___ | | | / ___ ( (_| | ____| |    
+ \_____/|_|   \___/|____/|  __/     |_|_|_\_____|_| |_\_____|\___ |_____)_|    
+                         |_|                                (_____|                 
+\e[0m"
+
+# Downloading the tool from GitHub
+echo -e "\e[94mDownloading the tool from GitHub...\e[0m"
+curl -s https://raw.githubusercontent.com/GrimalDev/group-manager/main/groupmng.py -o groupmng.py
+echo -e "\e[92mDone\e[0m\n"
+
+# Installing the tool
+echo -e "\e[94mInstalling the tool...\e[0m"
 sudo cp groupmng.py /usr/local/bin/groupmng
 sudo rm groupmng.py 
 sudo chmod +x /usr/local/bin/groupmng
-echo "Done\n"
+echo -e "\e[92mDone\e[0m\n"
